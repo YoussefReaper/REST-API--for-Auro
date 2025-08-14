df69b60fa8c9014bef003054280bca29bb600562
YoussefElAraby <arabymohammedyoussef123@gmail.com>
2025-08-14 09:42:58 +0300

    testing

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,8 +18,9 @@ exports.register = async (req, res) => {
         const hashed = await bcrypt.hash(password, 10);
         const newUser = new User({ username, password: hashed, email, emailVerificationToken: verificationToken, customization: {}});
         newUser.emailVerificationExpires = Date.now() + 24 * 60 * 60 * 1000;
+        const token = jwt.sign({ username: newUser.username, id: newUser._id}, KEY, {expiresIn: '1h'});
         await newUser.save();
-        res.status(201).json({message: 'User created'});
+        res.status(201).json({message: 'User created', token});
     } catch(err) {
         res.status(500).json({message: 'Server error', error: err.message});
     }
@@ -32,7 +33,7 @@ exports.login = async (req, res) => {
     try {
         const user = await User.findOne({username});
         if(!user) return res.status(401).json({message:'Invalid credentials'});
-        if (user.emailConfirmed !== true) return res.status(402).json({message: 'Email not confirmed'});
+        if (user.isVerified !== true) return res.status(402).json({message: 'Email not verified'});
         const valid = await bcrypt.compare(password, user.password);
         if(!valid) return res.status(401).json({message: 'Invalid credentials'});
         const token = jwt.sign({ username: user.username, id: user._id}, KEY, {expiresIn: '1h'});
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,6 +1,8 @@
 const {Resend} = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+console.log('Resend initialized with API key:', resend);
+
 async function sendEmail(to, subject, text) {
     await resend.emails.send({
         from: "AuroCore <noreply@aurocore.com>",
