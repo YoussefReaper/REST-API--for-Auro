f517e3ed5746caa3c0098ef34d8d8bfaf5a5ee6e
YoussefElAraby <arabymohammedyoussef123@gmail.com>
2025-08-16 11:42:19 +0300

    removed ? from req.cookis?.accessToken

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const KEY = process.env.KEY;
 
 module.exports = function authenticateToken(req, res, next) {
-    const token = req.cookies?.accessToken;
+    const token = req.cookies.accessToken;
     if (!token) return res.status(401).json({ message: 'Token missing' });
 
     jwt.verify(token, KEY, (err, user) => {
